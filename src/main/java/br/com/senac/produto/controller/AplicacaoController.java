296a7b005d2d3adace7aea739ccb64c1d79de022
Talisson-Rodrigues <talisontalison57@gmail.com>
2024-10-30 11:06:20 -0300

    fUNCIONANDO

--- src/main/java/br/com/senac/produto/controller/AplicacaoController.java
+++ src/main/java/br/com/senac/produto/controller/AplicacaoController.java
@@ -1,47 +1,50 @@
 package br.com.senac.produto.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import br.com.senac.produto.entity.Categoria;
+import br.com.senac.produto.entity.Produto;
 import br.com.senac.produto.repository.CategoriaRepository;
 import br.com.senac.produto.repository.ProdutoRepository;
+import jakarta.validation.Valid;
 
 @Controller
 public class AplicacaoController {
 
-    @Autowired
-    private CategoriaRepository categoriaRepository;
-    @Autowired
     private ProdutoRepository produtoRepository;
-    
-    public CategoriaRepository getCategoriaRepository() {
-        return categoriaRepository;
-    }
+    private CategoriaRepository categoriaRepository;
 
-    public void setCategoriaRepository(CategoriaRepository categoriaRepository) {
+    public AplicacaoController(ProdutoRepository produtoRepository,
+                                CategoriaRepository categoriaRepository) {
+        this.produtoRepository  = produtoRepository;
         this.categoriaRepository = categoriaRepository;
-    }
-
-    public ProdutoRepository getProdutoRepository() {
-        return produtoRepository;
-    }
-
-    public void setProdutoRepository(ProdutoRepository produtoRepository) {
-        this.produtoRepository = produtoRepository;
-    }
-
-    public AplicacaoController(CategoriaRepository categoriaRepository,
-                                 ProdutoRepository produtoRepository) {
-                                    
-        this.categoriaRepository = categoriaRepository;
-        this.produtoRepository = produtoRepository;
     }
 
     @GetMapping("/index")
-    public String showProdutoLista(Model model) {
+    public String showProdutosLista(Model model) {        
         model.addAttribute("produtos", produtoRepository.findAll());
         return "index";
+    }
+
+    @GetMapping("/cadproduto")
+    public String showCadProduto(Model model, Produto produto) {
+        model.addAttribute("categorias", categoriaRepository.findAll());        
+        return "cadProduto";        
+    }
+
+    @PostMapping("/addproduto")
+    public String addProduto(@Valid Produto produto, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return "cadProduto";
+        }
+        
+        produtoRepository.save(produto);
+        return "redirect:/index";
     }
 }
