cc9d1c3faf1df2f409169cdd888c4be7c321c117
Talisson-Rodrigues <talisontalison57@gmail.com>
2024-10-30 08:58:43 -0300

    Tá bom

---
+++ src/main/java/br/com/senac/produto/bd/estoque_insert.sql
@@ -0,0 +1,19 @@
+use estoque;
+
+INSERT INTO produto (preco, categoria, data_cadastro, nome) VALUES (99.90, 'Eletrônicos', '2024-10-18', 'Fone de Ouvido');
+INSERT INTO produto (preco, categoria, data_cadastro, nome) VALUES (2499.00, 'Informática', '2024-10-17', 'Notebook');
+INSERT INTO produto (preco, categoria, data_cadastro, nome) VALUES (199.99, 'Esporte', '2024-10-16', 'Tênis de Corrida');
+INSERT INTO produto (preco, categoria, data_cadastro, nome) VALUES (349.90, 'Casa', '2024-10-15', 'Aspirador de Pó');
+INSERT INTO produto (preco, categoria, data_cadastro, nome) VALUES (49.90, 'Livros', '2024-10-14', 'Romance');
+INSERT INTO produto (preco, categoria, data_cadastro, nome) VALUES (599.99, 'Moda', '2024-10-13', 'Jaqueta de Couro');
+INSERT INTO produto (preco, categoria, data_cadastro, nome) VALUES (39.90, 'Brinquedos', '2024-10-12', 'Boneca');
+INSERT INTO produto (preco, categoria, data_cadastro, nome) VALUES (99.90, 'Cozinha', '2024-10-11', 'Liquidificador');
+INSERT INTO produto (preco, categoria, data_cadastro, nome) VALUES (49.90, 'Papelaria', '2024-10-10', 'Caderno');
+INSERT INTO produto (preco, categoria, data_cadastro, nome) VALUES (299.00, 'Beleza', '2024-10-09', 'Secador de Cabelo');
+INSERT INTO produto (preco, categoria, data_cadastro, nome) VALUES (129.90, 'Automotivo', '2024-10-08', 'Bateria de Carro');
+INSERT INTO produto (preco, categoria, data_cadastro, nome) VALUES (999.90, 'Escritório', '2024-10-07', 'Cadeira Executiva');
+INSERT INTO produto (preco, categoria, data_cadastro, nome) VALUES (79.90, 'Pet Shop', '2024-10-06', 'Casinha de Cachorro');
+INSERT INTO produto (preco, categoria, data_cadastro, nome) VALUES (49.90, 'Jardinagem', '2024-10-05', 'Regador');
+INSERT INTO produto (preco, categoria, data_cadastro, nome) VALUES (89.90, 'Saúde', '2024-10-04', 'Medidor de Pressão');
+
+select * from produto;
